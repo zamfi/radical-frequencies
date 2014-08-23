7a2201ebcfb5f568934113bf7db3facc82da0157
Gil Zamfirescu <gzamfire@Gils-MacBook-Pro.local>
2014-08-23 10:41:13 -0700

    Initial Commit

---
+++ Radical Frequencies/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Radical Frequencies
+//
+//  Created by Gil Zamfirescu on 8/23/14.
+//  Copyright (c) 2014 TechLiminal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Radical FrequenciesTests/Radical_FrequenciesTests.m
@@ -0,0 +1,34 @@
+//
+//  Radical_FrequenciesTests.m
+//  Radical FrequenciesTests
+//
+//  Created by Gil Zamfirescu on 8/23/14.
+//  Copyright (c) 2014 TechLiminal. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Radical_FrequenciesTests : XCTestCase
+
+@end
+
+@implementation Radical_FrequenciesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
