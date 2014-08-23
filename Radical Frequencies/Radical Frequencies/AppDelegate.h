f8917c30a564e672aa5ce18bb52934ad0c55a6e9
Gil Zamfirescu <gzamfire@Gils-MacBook-Pro.local>
2014-08-23 15:50:36 -0700

    Adding arrow-1

---
+++ Radical Frequencies/Radical Frequencies/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Radical Frequencies
+//
+//  Created by Gil Zamfirescu on 8/23/14.
+//  Copyright (c) 2014 TechLiminal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
