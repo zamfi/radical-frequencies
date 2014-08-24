1b6ea11876d18cba92d770e659da5402b845b5a2
J.D. Zamfirescu-Pereira <jd@zamfi.net>
2014-08-23 20:55:41 -0700

    Using better temporary hostname

--- Radical Frequencies/RFNetworkHandler.m
+++ Radical Frequencies/RFNetworkHandler.m
@@ -17,8 +17,8 @@
     self.lastTagTime = [[NSNumber numberWithDouble:[[NSDate date] timeIntervalSince1970]*1000] unsignedLongLongValue];
     self.timer = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(pingServerForTags) userInfo:nil repeats:YES];
     
-    self.checkUrl = [NSString stringWithFormat:@"http://jd-mbp.local:8000/checkForTags?shared_secret=%@", [@"O[RVeWmPrm4OIzmXz&C}w-CM!m:f7*I" stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet characterSetWithCharactersInString:@"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-_.!~*'()"]]];
-    self.displayDataUrl = [NSString stringWithFormat:@"http://jd-mbp.local:8000/getMemberDisplayData?shared_secret=%@", [@"O[RVeWmPrm4OIzmXz&C}w-CM!m:f7*I" stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet characterSetWithCharactersInString:@"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-_.!~*'()"]]];
+    self.checkUrl = [NSString stringWithFormat:@"http://amulet.zamfi.net:8000/checkForTags?shared_secret=%@", [@"O[RVeWmPrm4OIzmXz&C}w-CM!m:f7*I" stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet characterSetWithCharactersInString:@"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-_.!~*'()"]]];
+    self.displayDataUrl = [NSString stringWithFormat:@"http://amulet.zamfi.net:8000/getMemberDisplayData?shared_secret=%@", [@"O[RVeWmPrm4OIzmXz&C}w-CM!m:f7*I" stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet characterSetWithCharactersInString:@"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-_.!~*'()"]]];
     return self;
 }
 
